d8af15742e5a60cd3ce4dae8d8af1736f7100d77
Marissa Zhong <mwz2101@columbia.edu>
2018-07-19 19:44:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  
+  function konamiCode(e) {
+    const key = parstInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Congratulations!");
+        index=0;
+      }
+    } else {
+      index=0;
+    }
+  }
 }
