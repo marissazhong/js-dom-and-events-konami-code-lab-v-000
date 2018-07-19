fbbe4c4098af743856a75015341b07e8aedf4b3c
Marissa Zhong <mwz2101@columbia.edu>
2018-07-19 19:53:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.body.addEventListener('keydown', (e) {
+  document.body.addEventListener('keydown', (e) => {
     const key = parstInt(e.detail || e.which);
     if (key === code[index]) {
       index++;
